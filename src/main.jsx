3b1cc77758b52ba6dd3cb07d8820550a5eec2b0b
baboshi <abdelhameid@abdelhameid>
2023-08-29 05:20:14 +0200

    search bar functionality added

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ const appStyle={
   textAlign:"center",
   fontSize:"20px",
   marginTop:"20px",
-  marginRight:"30px",
-  marginLeft:"30px"
+  marginRight:"10%",
+  marginLeft:"10%"
 
 }
 
@@ -40,7 +40,8 @@ const countryStyle={
 
   textAlign:"left",
   backgroundColor:"#FBEAEB",
-  color:"#2F3C7E"
+  color:"#2F3C7E",
+  cursor:"pointer"
 
   
 }
@@ -51,14 +52,18 @@ const Header=(props)=>{
   <header style={HeaderStyle}>
     <h1>World Coutries <br/>Data</h1>
     <p>Currently We have {props.countriesData.length} Countries</p>
-    <p> X satified the search criteria</p>
+    {/* <p> X satified the search criteria</p> */}
   </header>
   )
 }
-const SearchBar=()=>{
+const SearchBar=({setSearch})=>{
+  function handleChange(e){
+    const value=e.target.value
+    setSearch(value)
+  }
   return(
     <div>
-          <input autoFocus placeholder='Search Countries By' type="text" style={searchBarStyle}/>
+          <input autoFocus placeholder='Search Countries By' type="text" style={searchBarStyle} onChange={handleChange}/>
     </div>
   )
 }
@@ -70,18 +75,29 @@ const Country=({countryData:{name,capital,flags,population,languages}})=>{
     <div style={{align:"center" ,marginLeft:"25%"}}>
       <img src={flags.png} alt={name.common} />
     </div>
-    <h3>Name: {name.common}</h3>
-    <h3>Capital: {capital.join(",")}</h3>
-    <h3>Languages: {Object.keys(languages).join(',')}</h3>
-    <h3>Population: {population}</h3>
+    <h3 style={{color:"black"}}>Name: <span style={{color:"grey"}}>{name.common}</span></h3>
+    <h3 style={{color:"black"}}>Capital:    <span style={{color:"grey"}}>{capital.join(",")}</span></h3>
+    <h3 style={{color:"black"}}>Languages:  <span style={{color:"grey"}}>{Object.keys(languages).join(',')}</span></h3>
+    <h3 style={{color:"black"}}>Population: <span style={{color:"grey"}}>{population.toLocaleString("en-US")}</span></h3>
   </div>
   )
 }
-const CountriesList=({countriesData})=>{
-  console.log(countriesData)
+const CountriesList=({countriesData, search})=>{
+  let countries
+  if(search){
+  const filteredCountries=countriesData.filter((country)=>{
+    console.log(country)
+    const countryName=country.name.common.toUpperCase()
+    return countryName.includes(search.toUpperCase())
+  })
+   countries=filteredCountries.map(country=><Country countryData={country} key={country}/>)
+  }
+  else{
+    countries=countriesData.map(country=><Country countryData={country} key={country}/>)
+  }
   return(
   <div style={countriesListStyle}>
-    {countriesData.map(country=><Country countryData={country} key={country}/>)}
+    {countries}
   </div>
   )
   
@@ -89,8 +105,9 @@ const CountriesList=({countriesData})=>{
 
 
 const App=()=>{
+  
   const [data,setData]=useState([])
-
+  const [search,setSearh]=useState('')
   useEffect(()=>{
     fetchData()
   },[])
@@ -110,8 +127,8 @@ const App=()=>{
   return (
     <div style={appStyle}>
       <Header countriesData={data}/>
-      <SearchBar/>
-      <CountriesList countriesData={data}/>
+      <SearchBar setSearch={setSearh}/>
+      <CountriesList countriesData={data} search={search}/>
       
     </div>
   )
